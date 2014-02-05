a9a5a4b16d1b1d89344af096b2b13dfd945626f8
Clay Goddard <goddardc@gatech.edu>
2014-02-05 13:25:08 -0500

    Added back in test, user needed to be signed in

--- test/controllers/organizations_controller_test.rb
+++ test/controllers/organizations_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
+include Devise::TestHelpers
 
 class OrganizationsControllerTest < ActionController::TestCase
-  #test "should get new" do
-  #  get :new
-  #  assert_response :success
-  #end
+  test "should get new" do
+    sign_in User.first
+    get :new
+    assert_response :success
+  end
 
 end
