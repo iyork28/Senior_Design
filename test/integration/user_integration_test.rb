78f0efc9a9869fa41939c3e73baa918f2a5653a9
Clay Goddard <goddardc@gatech.edu>
2014-02-05 14:43:07 -0500

    Initial addition of integration tests with capybara

---
+++ test/integration/user_integration_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class OrganizationTest < ActionDispatch::IntegrationTest
+  test "user sign in fail" do
+    visit new_user_session_path
+    fill_in 'user_email', with: 'clay@goddard.com'
+    fill_in 'user_password', with: '123'
+    click_button 'Sign in'
+    save_and_open_page
+    assert current_path == new_user_session_path
+  end
+end
