43100f146f1c3befaaa06feadceb12f96d959030
Ian York <iyork28@gmail.com>
2014-02-20 20:35:24 -0500

    Added some support for flash messages

--- db/schema.rb
+++ db/schema.rb
@@ -40,21 +40,8 @@ ActiveRecord::Schema.define(version: 20140218170221) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "",                                         null: false
-    t.string   "encrypted_password",     default: "",                                         null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,                                          null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "password",               default: "9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684"
+    t.string   "password",   default: "9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684"
   end
-
-  add_index "organizations", ["email"], name: "index_organizations_on_email", unique: true
-  add_index "organizations", ["reset_password_token"], name: "index_organizations_on_reset_password_token", unique: true
 
   create_table "organizations_users", force: true do |t|
     t.integer "user_id"
