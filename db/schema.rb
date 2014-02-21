8cbf8053b2e493a98651c97c3b26e1513d5408a1
Ian York <iyork28@gmail.com>
2014-02-21 12:37:11 -0500

    Pushing updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140218170221) do
+ActiveRecord::Schema.define(version: 20140221173156) do
 
   create_table "charges", force: true do |t|
     t.string   "description"
